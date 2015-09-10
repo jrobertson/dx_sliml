b0730498551de675b267fbd893238dbd4c62506b
Your Name <you@example.com>
2015-09-10 18:35:32 +0100

    1st commit

---
+++ dx_sliml.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'dx_sliml'
+  s.version = '0.1.0'
+  s.summary = 'Uses a Sliml like format to transform a Dynarex document to HTML.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/dx_sliml.rb']
+  s.add_runtime_dependency('dynarex', '~> 1.5', '>=1.5.34')
+  s.signing_key = '../privatekeys/dx_sliml.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/dx_sliml'
+end
---
+++ lib/dx_sliml.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+
+# file: dx_sliml.rb
+
+require 'dynarex'
+
+
+class DxSliml
+
+  attr_reader :to_xslt, :to_html
+
+  def initialize(sliml, dx)
+
+    @dx = dx.is_a?(Dynarex) ? dx : Dynarex.new(dx)
+    xml = LineTree.new(sliml).to_xml declaration: false, pretty: true
+    @recxsl = xml.gsub(/\$(\w+)/, '<xsl:value-of select="\1"/>')
+    @to_xslt = build_xslt
+
+    xslt  = Nokogiri::XSLT(@to_xslt)
+    @to_html = xslt.transform(Nokogiri::XML(@dx.to_xml))
+
+  end
+
+
+  private
+
+  def build_xslt()
+
+    schema = @dx.schema
+    rootname, recname = schema.split('/').map{|x| x[/\w+/]}
+
+    xml = RexleBuilder.new
+    raw_a = xml.xsl_stylesheet(xmlns_xsl: \
+                     "http://www.w3.org/1999/XSL/Transform", version: "1.0") do
+      xml.xsl_output(method: "xml", indent: "yes", \
+                                              :"omit-xml-declaration" => "yes")
+
+      xml.xsl_template(match: rootname) do
+        xml.html do
+          xml.head do
+            xml.title(@dx.title) if @dx.summary[:title] 
+          end 
+          xml.body do
+            xml.header do
+              xml.xsl_apply_templates(select: 'summary')
+            end
+            xml.main do
+              xml.xsl_apply_templates(select: 'records/' + recname)
+            end
+          end 
+        end
+      end
+
+      xml.xsl_template(match: rootname + '/summary') do
+        
+        xml.dl do
+          xml.xsl_for_each(select: '*') do
+            xml.dt do
+              xml.xsl_value_of(select: 'name()')
+            end
+            xml.dd do
+              xml.xsl_value_of(select: '.')
+            end
+          end
+        end
+      end
+
+      xml.xsl_template(match: 'records/' + recname) do
+
+        xml.rec_template
+
+      end
+
+    end
+
+    xml2 = Rexle.new(raw_a).xml(pretty: true).gsub('xsl_apply_templates',\
+        'xsl:apply-templates').gsub('xsl_value_of','xsl:value-of').\
+        gsub('xsl_template','xsl:template').\
+        gsub('xmlns_xsl','xmlns:xsl').gsub('xsl_for_each','xsl:for-each').\
+        gsub('xsl_','xsl:')
+
+    xml2.sub('<rec_template/>', @recxsl)
+  end
+
+end
